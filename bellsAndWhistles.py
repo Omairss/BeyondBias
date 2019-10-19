f9acd0350dcb7d9cb7ce271efeee2d0c47e90812
Gayatri R <42282250+grajan22@users.noreply.github.com>
2019-10-19 19:08:34 -0400

    Update bellsAndWhistles.py

--- bellsAndWhistles.py
+++ bellsAndWhistles.py
@@ -5,16 +5,22 @@ import requests
 from bs4 import BeautifulSoup
 def getReadTime(body):
     wc = len(body.split())
-    return wc/100
+    return wc/200
 def getToneScore(body):
     opinion = TextBlob(body)
     sentiment = opinion.sentiment.polarity
-    return sentiment*100
-def getAuthorScore():
-    return 85.3
-#def getSourceScore()
+    return int(sentiment*100)
+def getSourceScore(article):
+    html = article.html
+    text = article.text
+    #get number of long quotes
+    nq = text.count("\"")/2
+    #get number of links
+    nl = html.count("<a href")
+    returnVal=nq+nl
+    return (str(returnVal*5)+"%")
 if __name__ == '__main__':
-    url = 'https://www.newyorker.com/news/dispatch/why-lebanons-people-are-turning-on-their-politicians'
+    url = 'https://www.newyorker.com/books/page-turner/how-jane-vonnegut-made-kurt-vonnegut-a-writer'
     article = Article(url)
     article.download()
     article.parse()
@@ -24,3 +30,4 @@ if __name__ == '__main__':
     authors=""
     print(getToneScore(article.text))
     print(str(getReadTime(article.text))+" minutes")
+    print(getSourceScore(article))
